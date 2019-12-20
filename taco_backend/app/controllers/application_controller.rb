47975650864d420bb334a22bf76410c6277fd52b
Raul <raulsg93@gmail.com>
2019-12-20 14:16:36 -0600

    changed made to applicationController to add auth

--- taco_backend/app/controllers/application_controller.rb
+++ taco_backend/app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
+    before_action :authenticated
+
+    def current_user
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        else
+            # nil
+        end
+    end
+
+    def logged_in?
+        !current_user
+    end
+
+    def authenticated
+        render json: 'authenticate is successful'
+    end
+    
 end
