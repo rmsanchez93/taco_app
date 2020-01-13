6178451389d56be52baf2ad3b0baa2eab77eb35f
Raul <raulsg93@gmail.com>
2020-01-13 14:17:34 -0600

    sessions created and deleted on log in and log out

--- taco_backend/app/controllers/sessions_controller.rb
+++ taco_backend/app/controllers/sessions_controller.rb
@@ -2,20 +2,24 @@ class SessionsController < ApplicationController
     skip_before_action :authenticated, only: [:new, :create]
 
     def new
+        render json: 'you hit the login route'
     end
 
     def create
-        @user = User.find_by(name: params[:name])
-
-        if @user.authenticated(params[:password])
-            session[:user_id] = @user.user_id
-            render json: @user
+        @user = User.find_by(email: params[:email])
+        # byebug
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            render json: @user, :except => [:created_at, :updated_at]
         else
-            render json: 'could not authenticate user'
+            render json: "incorrect email or password"
         end
     end
 
     def destroy
+        puts session[:user_id]
+        # byebug
         session.delete(:user_id)
+        render json: 'logged out successful'
     end
 end
