6d6759cd68b8a9998bc182e35fd5438a68e05442
Raul <raulsg93@gmail.com>
2020-02-16 00:54:08 -0600

    stopping point before merging

--- taco_backend/db/seeds.rb
+++ taco_backend/db/seeds.rb
@@ -14,18 +14,21 @@ Review.destroy_all
 raul = User.create(name: 'Raul', email: 'raulsg93@gmail.com', phone: '832-643-9708', password: 'tacos', address: '7902 Corrian Park Circle', city: 'Houston', state: 'TX', user_name:'raulabear', role: 0)
 israel = User.create(name: 'Israel', email: 'Israel.pro1994@gmail.com', phone: '832-643-8895', password: '1234', address: '7902 Corrian Park Circle', city: 'Houston', state: 'TX', user_name: 'is09_Real13', role:1)
 gadi = User.create(name: 'Gadi', email: 'gadi.hidalgo@gmail.com', phone: '777-777-7777', password: 'gadi', address: '11111 Saathoff Drive', city: 'Houston', state: 'TX', user_name: 'crunchy_bear', role: 1)
+truck_owner = User.create(name: 'Truck Owner Test', email: 'owner@gmail.com', phone: 'xxx-xxx-xxxx', password: 'owner', address: 'some fuckig place', city: 'Houston', state: 'TX', user_name: 'owner', role: 0)
 
 
 labala = Tacotruck.create(name: 'La Bala', owner: raul.name, address: '89056 someplace st.', city:'Houston', state: 'TX', email: 'labala@labala.com' , phone: '789-789-7676', user_id: raul.id)
+truck_test = Tacotruck.create(name: 'Truck Owner', owner: truck_owner.name, address: 'truck address test', city:'Houston', state: 'TX', email: 'footrucktest' , phone: '789-789-7676', user_id: truck_owner.id)
 
 rev1 = Review.create(user_id: raul.id, tacotruck_id: labala.id, review: 'this place was aight I felt it could used green sauce', rating: 3)
 rev2 = Review.create(user_id: israel.id, tacotruck_id: labala.id, review: 'this place was straight garbage', rating: 1)
 rev3 = Review.create(user_id: gadi.id, tacotruck_id: labala.id, review: 'this place could be better', rating: 2)
 
 menu_test = Menu.create(tacotruck_id: labala.id)
+truck_menu_test = Menu.create(tacotruck_id: truck_test.id)
 
 menu_item_test = MenuItem.create(name: 'tacos de pastor', price: '3.25', menu_id:menu_test.id, tacotruck_id: labala.id, description: '3 tacos made with pastor pork', favorite: false, special: true)
-
+menu_item_test2 = MenuItem.create(name: 'veggies tacos', price: '3.25', menu_id:truck_menu_test.id, tacotruck_id: truck_test.id, description: 'veggies tacos full of veggie goodness', favorite: true, special: true)
 
 
 puts 'this is seeded'
